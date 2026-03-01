06f6e632cc54818f45d1f38a65f52e487c552853
SolidRhino <SolidRhino@users.noreply.github.com>
2026-03-01 16:05:12 +0100

    feat: set scrolloff to 8 for more scroll context

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.scrolloff = 8
+
 -- Make mise-managed tools available to Neovim (LSPs, formatters, linters, etc.)
 -- https://mise.jdx.dev/ide-integration.html#neovim
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
