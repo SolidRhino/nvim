393381a2df44d702c0d9a975c8acfa6cebe240ca
SolidRhino <SolidRhino@users.noreply.github.com>
2026-03-01 15:47:47 +0100

    feat: add mise shims to PATH for tool availability in Neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Make mise-managed tools available to Neovim (LSPs, formatters, linters, etc.)
+-- https://mise.jdx.dev/ide-integration.html#neovim
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
